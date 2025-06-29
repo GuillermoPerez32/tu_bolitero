1662124cf3330b5b7b01d24f28bcd0245f1aca23
Luis Guillermo Pérez Rodríguez <luisguillermo.rodriguez32@gmail.com>
2025-06-29 00:38:53 -0400

    fix app label

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 const host = 'https://www.tuboliteros.com';
 const horarios = ['general', 'mañana', 'mediodía', 'tarde', 'noche'];
-const currentApkVersion = '1.3.1';
+const currentApkVersion = '1.3.20';
 
 const appTitle = 'Tu Bolitero';
