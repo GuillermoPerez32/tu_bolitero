4971371d5ed8ada6da31a780f70d86db78537ece
Luis Guillermo Pérez Rodríguez <luisguillermo.rodriguez32@gmail.com>
2025-07-03 11:39:13 -0400

    fix login

--- lib/ui/screens/auth/login_screen.dart
+++ lib/ui/screens/auth/login_screen.dart
@@ -34,114 +34,120 @@ class _LoginScreenState extends State<LoginScreen> {
       },
       builder: (context, state) {
         return Scaffold(
-          body: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Iniciar Sesión',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-                  ),
-                  const SizedBox(height: 20),
-                  Image.asset('assets/login_banner.png'),
-                  const SizedBox(height: 20),
-                  TextField(
-                    autocorrect: false,
-                    controller: _usernameController,
-                    decoration: const InputDecoration(
-                      labelText: 'Usuario',
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 32.0, horizontal: 20),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Iniciar Sesión',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                  TextField(
-                    autocorrect: false,
-                    controller: _passwordController,
-                    decoration: const InputDecoration(
-                      labelText: 'Contraseña',
+                    const SizedBox(height: 20),
+                    Image.asset(
+                      'assets/login_banner.png',
+                      height: 200,
                     ),
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 20),
-                  state.maybeWhen(
-                    orElse: () => const SizedBox.shrink(),
-                    error: (message, user) => Text(message,
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.error,
-                        )),
-                  ),
-                  const SizedBox(height: 20),
-                  // Row(
-                  //   mainAxisAlignment: MainAxisAlignment.end,
-                  //   children: [
-                  //     InkWell(
-                  //       onTap: () {
-                  //         context.go('/login/forgot-password');
-                  //       },
-                  //       child: Text(
-                  //         '¿Olvidaste tu contraseña?',
-                  //         style: TextStyle(
-                  //           color: Theme.of(context).colorScheme.onSecondary,
-                  //           fontWeight: FontWeight.w400,
-                  //         ),
-                  //       ),
-                  //     ),
-                  //   ],
-                  // ),
-                  const SizedBox(height: 10),
-                  FilledButton(
-                    onPressed: _isLoading
-                        ? null
-                        : () {
-                            setState(() {
-                              _isLoading = true;
-                            });
-                            final username = _usernameController.text;
-                            final password = _passwordController.text;
-                            authCubit.login(
-                                username: username.trim(), password: password);
-                          },
-                    style: FilledButton.styleFrom(
-                      minimumSize: const Size(double.infinity, 48),
-                    ),
-                    child: _isLoading
-                        ? const SizedBox(
-                            width: 24,
-                            height: 24,
-                            child: CircularProgressIndicator(
-                              strokeWidth: 2,
-                            ),
-                          )
-                        : const Text('Iniciar Sesión'),
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        '¿No tienes cuenta?',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                        ),
+                    const SizedBox(height: 20),
+                    TextField(
+                      autocorrect: false,
+                      controller: _usernameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Usuario',
                       ),
-                      const SizedBox(width: 4),
-                      GestureDetector(
-                        onTap: () {
-                          context.go('/login/register');
-                        },
-                        child: Text(
-                          'Registrate',
+                    ),
+                    const SizedBox(height: 20),
+                    TextField(
+                      autocorrect: false,
+                      controller: _passwordController,
+                      decoration: const InputDecoration(
+                        labelText: 'Contraseña',
+                      ),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 20),
+                    state.maybeWhen(
+                      orElse: () => const SizedBox.shrink(),
+                      error: (message, user) => Text(message,
                           style: TextStyle(
-                            color: Theme.of(context).colorScheme.onSecondary,
+                            color: Theme.of(context).colorScheme.error,
+                          )),
+                    ),
+                    const SizedBox(height: 20),
+                    // Row(
+                    //   mainAxisAlignment: MainAxisAlignment.end,
+                    //   children: [
+                    //     InkWell(
+                    //       onTap: () {
+                    //         context.go('/login/forgot-password');
+                    //       },
+                    //       child: Text(
+                    //         '¿Olvidaste tu contraseña?',
+                    //         style: TextStyle(
+                    //           color: Theme.of(context).colorScheme.onSecondary,
+                    //           fontWeight: FontWeight.w400,
+                    //         ),
+                    //       ),
+                    //     ),
+                    //   ],
+                    // ),
+                    const SizedBox(height: 10),
+                    FilledButton(
+                      onPressed: _isLoading
+                          ? null
+                          : () {
+                              setState(() {
+                                _isLoading = true;
+                              });
+                              final username = _usernameController.text;
+                              final password = _passwordController.text;
+                              authCubit.login(
+                                  username: username.trim(),
+                                  password: password);
+                            },
+                      style: FilledButton.styleFrom(
+                        minimumSize: const Size(double.infinity, 48),
+                      ),
+                      child: _isLoading
+                          ? const SizedBox(
+                              width: 24,
+                              height: 24,
+                              child: CircularProgressIndicator(
+                                strokeWidth: 2,
+                              ),
+                            )
+                          : const Text('Iniciar Sesión'),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          '¿No tienes cuenta?',
+                          style: TextStyle(
                             fontWeight: FontWeight.w400,
                           ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
+                        const SizedBox(width: 4),
+                        GestureDetector(
+                          onTap: () {
+                            context.go('/login/register');
+                          },
+                          child: Text(
+                            'Registrate',
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.onSecondary,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
--- lib/ui/screens/auth/register_screen.dart
+++ lib/ui/screens/auth/register_screen.dart
@@ -48,120 +48,124 @@ class _RegisterScreenState extends State<RegisterScreen> {
       },
       builder: (context, state) {
         return Scaffold(
-          body: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Registrarse',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-                  ),
-                  const SizedBox(height: 20),
-                  Image.asset('assets/login_banner.png'),
-                  const SizedBox(height: 20),
-                  TextField(
-                    autocorrect: false,
-                    controller: _usernameController,
-                    decoration: const InputDecoration(
-                      labelText: 'Usuario',
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 32.0, horizontal: 20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Registrarse',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                  TextField(
-                    autocorrect: false,
-                    controller: _emailController,
-                    decoration: const InputDecoration(
-                      labelText: 'Correo Electrónico',
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  const SizedBox(height: 20),
-                  TextField(
-                    autocorrect: false,
-                    controller: _passwordController,
-                    decoration: const InputDecoration(
-                      labelText: 'Contraseña',
-                    ),
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 20),
-                  TextField(
-                    autocorrect: false,
-                    controller: _confirmPasswordController,
-                    decoration: const InputDecoration(
-                      labelText: 'Confirmar Contraseña',
-                    ),
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 20),
-                  state.maybeWhen(
-                    orElse: () => const SizedBox.shrink(),
-                    error: (message, user) => Text(message,
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.error,
-                        )),
-                  ),
-                  const SizedBox(height: 10),
-                  FilledButton(
-                    onPressed: _isLoading
-                        ? null
-                        : () {
-                            setState(() {
-                              _isLoading = true;
-                            });
-                            final username = _usernameController.text;
-                            final email = _emailController.text;
-                            final password = _passwordController.text;
-                            final confirmPassword =
-                                _confirmPasswordController.text;
-                            authCubit.register(
-                                username: username,
-                                email: email,
-                                password: password,
-                                confirmPassword: confirmPassword);
-                          },
-                    style: FilledButton.styleFrom(
-                      minimumSize: const Size(double.infinity, 48),
-                    ),
-                    child: _isLoading
-                        ? const SizedBox(
-                            width: 24,
-                            height: 24,
-                            child: CircularProgressIndicator(
-                              strokeWidth: 2,
-                            ),
-                          )
-                        : const Text('Registrarse'),
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        '¿Ya tienes cuenta?',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                        ),
+                    const SizedBox(height: 20),
+                    Image.asset('assets/login_banner.png'),
+                    const SizedBox(height: 20),
+                    TextField(
+                      autocorrect: false,
+                      controller: _usernameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Usuario',
                       ),
-                      const SizedBox(width: 4),
-                      GestureDetector(
-                        onTap: () {
-                          context.go('/login');
-                        },
-                        child: Text(
-                          'Iniciar Sesión',
+                    ),
+                    const SizedBox(height: 20),
+                    TextField(
+                      autocorrect: false,
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        labelText: 'Correo Electrónico',
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(height: 20),
+                    TextField(
+                      autocorrect: false,
+                      controller: _passwordController,
+                      decoration: const InputDecoration(
+                        labelText: 'Contraseña',
+                      ),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 20),
+                    TextField(
+                      autocorrect: false,
+                      controller: _confirmPasswordController,
+                      decoration: const InputDecoration(
+                        labelText: 'Confirmar Contraseña',
+                      ),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 20),
+                    state.maybeWhen(
+                      orElse: () => const SizedBox.shrink(),
+                      error: (message, user) => Text(message,
                           style: TextStyle(
-                            color: Theme.of(context).colorScheme.onSecondary,
+                            color: Theme.of(context).colorScheme.error,
+                          )),
+                    ),
+                    const SizedBox(height: 10),
+                    FilledButton(
+                      onPressed: _isLoading
+                          ? null
+                          : () {
+                              setState(() {
+                                _isLoading = true;
+                              });
+                              final username = _usernameController.text;
+                              final email = _emailController.text;
+                              final password = _passwordController.text;
+                              final confirmPassword =
+                                  _confirmPasswordController.text;
+                              authCubit.register(
+                                  username: username,
+                                  email: email,
+                                  password: password,
+                                  confirmPassword: confirmPassword);
+                            },
+                      style: FilledButton.styleFrom(
+                        minimumSize: const Size(double.infinity, 48),
+                      ),
+                      child: _isLoading
+                          ? const SizedBox(
+                              width: 24,
+                              height: 24,
+                              child: CircularProgressIndicator(
+                                strokeWidth: 2,
+                              ),
+                            )
+                          : const Text('Registrarse'),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          '¿Ya tienes cuenta?',
+                          style: TextStyle(
                             fontWeight: FontWeight.w400,
                           ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
+                        const SizedBox(width: 4),
+                        GestureDetector(
+                          onTap: () {
+                            context.go('/login');
+                          },
+                          child: Text(
+                            'Iniciar Sesión',
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.onSecondary,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
